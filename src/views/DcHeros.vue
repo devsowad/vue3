03e0f8329b6439ca66c3231ad6a8f104ff1c92d9
Sowad <sowad1035@gmail.com>
2021-05-18 08:17:28 +0600

    Refracting dcHeros with composition api.

--- src/views/DcHeros.vue
+++ src/views/DcHeros.vue
@@ -1,6 +1,6 @@
 <template>
   <h2 class="w-full text-4xl mb-10 font-bold text-secondary text-center">
-    DC Heros
+    DC Heros ({{ totalHero }})
   </h2>
   <div class="w-full">
     <form
@@ -22,48 +22,51 @@
       </button>
     </form>
   </div>
-  <div
-    class="transition-all flex justify-between items-center group px-6 py-5 max-w-full mx-auto w-72 border border-primary border-opacity-25 cursor-pointer rounded select-none overflow-hidden space-y-1 hover:bg-white shadow-lg hover:border-transparent my-2"
-    v-for="(hero, index) in dcHeros"
-    :key="hero.name"
-  >
-    <p class="font-semibold text-lg text-primary group-hover:text-gray-900">
-      {{ hero.name }}
-    </p>
-    <button
-      @click="removeHero(index)"
-      class="bg-primary text-white text-sm font-semibold p-2 px-3 rounded-full shadow-md hover:bg-primary hover:bg-opacity-80 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 focus:ring-offset-purple-200 material-icons"
+  <div class="block">
+    <div
+      class="transition-all flex justify-between items-center group px-6 py-5 max-w-full mx-auto w-72 border border-primary border-opacity-25 cursor-pointer rounded select-none overflow-hidden space-y-1 hover:bg-white shadow-lg hover:border-transparent my-2"
+      v-for="(hero, index) in dcHeros"
+      :key="hero.name"
     >
-      close
-    </button>
+      <p class="font-semibold text-lg text-primary group-hover:text-gray-900">
+        {{ hero.name }}
+      </p>
+      <button
+        @click="removeHero(index)"
+        class="bg-primary text-white text-sm font-semibold p-2 px-3 rounded-full shadow-md hover:bg-primary hover:bg-opacity-80 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 focus:ring-offset-purple-200 material-icons"
+      >
+        close
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
+import { computed, ref } from "vue";
+
 export default {
-  data() {
-    return {
-      newHero: null,
-      dcHeros: [{ name: "Super Man" }],
-    };
-  },
-  methods: {
-    addHero() {
-      if (this.newHero) {
-        this.dcHeros.unshift({ name: this.newHero });
-        this.newHero = null;
+  setup() {
+    const newHero = ref(null);
+    const dcHeros = ref([{ name: "Super Man" }]);
+
+    const totalHero = computed({
+      get: () => dcHeros.value.length,
+    });
+
+    const addHero = () => {
+      if (newHero.value) {
+        dcHeros.value.unshift({ name: newHero.value });
+        newHero.value = null;
       } else {
         alert("Insert name");
       }
-    },
-    removeHero(index) {
-      this.dcHeros = this.dcHeros.filter((hero, i) => i !== index);
-    },
-  },
-  computed: {
-    totalHero() {
-      return this.dcHeros.length;
-    },
+    };
+
+    const removeHero = (index) => {
+      dcHeros.value = dcHeros.value.filter((hero, i) => i !== index);
+    };
+
+    return { newHero, dcHeros, totalHero, addHero, removeHero };
   },
 };
 </script>
